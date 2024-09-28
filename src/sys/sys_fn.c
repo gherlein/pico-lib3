0d468ca0b624592ed8406915f461b6cbd2601d7e
Greg Herlein <gherlein@herlein.com>
2024-09-28 07:39:23 -0700

    updated

--- src/sys/sys_fn.c
+++ src/sys/sys_fn.c
@@ -12,12 +12,18 @@
 #include "sys_gpio.h"
 #include "sys_time.h"
 
-
 #ifdef RP_PICO_W_BOARD
 #include "pico/cyw43_arch.h"
 #endif
 
 void sys_init() {
+    // workaround for a hardware debug problem
+    // https://forums.raspberrypi.com/viewtopic.php?t=363914
+    // also in openocd you need to us -c "set USE_CORE 0"  before rp2040.cfg is loaded per
+    // https://github.com/raspberrypi/debugprobe/issues/45
+    timer_hw->dbgpause = 0;
+    sleep_ms(150);
+
     // init stdio
     stdio_init_all();
 
